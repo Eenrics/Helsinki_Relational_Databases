365427279efc16047433fce3d8d23c1b8f824483
Ebenexer <ebenezeresh525@gmail.com>
2023-09-21 03:58:06 +0300

    migration reversing using unzum donw

--- part_3/example/controllers/notes.js
+++ part_3/example/controllers/notes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
-const jwt = require('jsonwebtoken')
 const {Op} = require('sequelize')
+const {tokenExtractor} = require('../utils/middleware')
 
 const { Note, User } = require('../models')
 const { SECRET } = require('../utils/config')
@@ -28,22 +28,6 @@ router.get('/', async (req, res) => {
   })
   res.json(notes)
 })
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      console.log(authorization.substring(7), SECRET)
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-    } catch (error) {
-      console.log(error)
-      return res.status(401).json({ error: 'token invalid' })
-    }
-  }  else {
-    return res.status(401).json({ error: 'token missing' })
-  }
-  next()
-}
 
 router.post('/', tokenExtractor, async (req, res) => {
   try {
